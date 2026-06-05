c4caa6e59d0147e7aed1878dc05690995ba06305
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-06-05 11:15:42 +0700

    feat: deklarasi register method di auth repo

--- src/domain/repositories/IAuthRepository.ts
+++ src/domain/repositories/IAuthRepository.ts
@@ -2,5 +2,9 @@ import { LoginResponse, RegisterResponse } from "../entities/Auth";
 
 export interface IAuthRepository {
   login(email: string, password: string): Promise<LoginResponse>;
-  
+  register(
+    username: string,
+    email: string,
+    password: string,
+  ): Promise<RegisterResponse>;
 }
