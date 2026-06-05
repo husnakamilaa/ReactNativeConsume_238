179e9a6a85c3b49ad345aafa00949898fde7d151
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-06-05 18:45:56 +0700

    feat: menambahkan komponen format text dan ui

---
+++ src/components/ThemedView.tsx
@@ -0,0 +1,16 @@
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "react-native";
+import { View, type ViewProps } from "react-native";
+
+export interface ThemedViewProps extends ViewProps {
+    lightColor?: string;
+    darkColor?: string;
+}
+
+export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+    const theme = useColorScheme() ?? 'light';
+    const backgroundColor = theme === 'light'
+    ? (lightColor ?? Colors.light.background) 
+    : (darkColor ?? Colors.dark.background);
+    return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+}
