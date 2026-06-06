2c9bf44cd6d4beb7c7abfea851f61d11c18b6c5e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-06-07 03:51:45 +0700

    fix: benerin import di edit hewan

--- src/app/main/edit_hewan.tsx
+++ src/app/main/edit_hewan.tsx
@@ -1,11 +1,20 @@
-import { HewanStatus } from "@/domain/entities/Hewan";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { HEWAN_STATUS_OPTIONS, HewanStatus } from "@/domain/entities/Hewan";
 import { useHewanViewModel } from "@/hooks/useHewanViewModel";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
 import {
-    Alert,
-    Platform
+  ActivityIndicator,
+  Alert,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function EditHewanScreen() {
   const {
