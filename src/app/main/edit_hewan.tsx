154a166feec52a5e6f02f0a594f2990fc573a853
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-06-07 03:38:14 +0700

    feat: nambahin use local param dan deklarasi variable yang berisi data dari add form

--- src/app/main/edit_hewan.tsx
+++ src/app/main/edit_hewan.tsx
@@ -3,7 +3,7 @@ import { ThemedView } from "@/components/themed-view";
 import { HEWAN_STATUS_OPTIONS, HewanStatus } from "@/domain/entities/Hewan";
 import { useHewanViewModel } from "@/hooks/useHewanViewModel";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
 import {
   ActivityIndicator,
@@ -17,5 +17,13 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function EditHewanScreen() {
-    
+    const { id, nama: namaParam, jenis: jenisParam, harga: hargaParam, tanggal_lahir, status: statusParam } = useLocalSearchParams<{
+    id: string;
+    nama: string;
+    jenis: string;
+    harga: string;
+    tanggal_lahir: string;
+    status: HewanStatus;
+  }>();
+
 }
